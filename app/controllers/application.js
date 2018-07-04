9a510d0e3b07bc9b036837b58adf63a1cf242338
blamens <blamens@nowhere.com>
2018-07-04 10:32:24 +0200

    First commit

---
+++ app/controllers/application.js
@@ -0,0 +1,13 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+	selectdOptions: undefined,
+
+	options: ['foo', 'bar', 'baz'],
+
+	actions: {
+		onChange() {
+
+		}
+	}
+});
